fae4dd3665e1e702ad2f53330a887c203e0ad66e
Nick Hynes <nhynes@oasislabs.com>
2019-07-31 20:34:43 +0000

    Install specific version of oasis-chain

--- installer/build.rs
+++ installer/build.rs
@@ -1,9 +1,16 @@
 fn main() {
-    let envs: std::collections::HashMap<String, String> = std::env::vars().collect();
-    let mut rustflags = envs.entry("RUSTFLAGS".to_string()).or_default();
+    let mut envs: std::collections::HashMap<String, String> = std::env::vars().collect();
+    let rustflags = envs.entry("RUSTFLAGS".to_string()).or_default();
     *rustflags = format!("{} -Ctarget-feature=+aes,+ssse3", rustflags);
     std::process::Command::new("cargo")
-        .args(&["install", "--force", "--git", "https://github.com/oasislabs/oasis-chain"])
+        .args(&[
+            "install",
+            "--force",
+            "--git",
+            "https://github.com/oasislabs/oasis-chain",
+            "--tag",
+            "v0.1.0",
+        ])
         .envs(envs)
         .output()
         .unwrap();
