db8d00272a03b73a3dc3ee515a41dd19d26f90d5
Peter Gilbert <pgilbert@oasislabs.com>
2019-08-02 16:59:25 -0700

    More unused deps

--- ekiden_keymanager/src/lib.rs
+++ ekiden_keymanager/src/lib.rs
@@ -1,11 +1,8 @@
 extern crate ekiden_crypto;
-extern crate failure;
 extern crate rand;
 extern crate rustc_hex;
 extern crate serde;
-extern crate serde_bytes;
 extern crate serde_derive;
-extern crate x25519_dalek;
 
 #[macro_use]
 mod api;
